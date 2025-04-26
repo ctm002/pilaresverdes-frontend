9d414919e4843ee35721c4817dfc25c42af4f57d
Carlos Rodrigo Tapia Molina <ctapia@transbank.cl>
2025-04-26 16:13:01 -0400

    fix: config

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
     e.preventDefault();
 
     try {
-      const response = await axios.post('http://localhost/api/v1/auth/signin', {
+      const response = await axios.post('/api/v1/auth/signin', {
         username,
         password
       });
--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://localhost:9090',
-  //       changeOrigin: true,
-  //       secure: false,
-  //     },
-  //   },
-  // },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://backend-api:9090',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
