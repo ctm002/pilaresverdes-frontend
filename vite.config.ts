28e37e31be8c3b09b4692cb18cead188153b3e19
Carlos Rodrigo Tapia Molina <ctapia@transbank.cl>
2025-04-28 08:40:40 -0400

    config proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import proxy from './vite.proxy.js';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    // proxy: {
-    //   '/api': {
-    //     target: 'http://localhost:9090',
-    //     changeOrigin: true,
-    //     secure: false,
-    //   },
-    // },
-    proxy: proxy,
+    proxy: {
+      '/api': {
+        target: 'http://backend-api:9090',
+        // target: 'http://localhost:9090',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 })
