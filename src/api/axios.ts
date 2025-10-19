09abd5e6663ed9c856e8c40b86595bc973a0bb05
Carlos Rodrigo Tapia Molina <ctapia@transbank.cl>
2025-10-19 07:21:11 -0300

    refresh token, no funciona, pero lo se

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,46 +1,53 @@
 import axios, { InternalAxiosRequestConfig } from "axios";
-// import { jwtDecode } from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 
 const api = axios.create({
   baseURL: "",
 });
 
-// interface JWTPayload {
-//   exp: number;
-// }
+interface JWTPayload {
+  exp: number;
+}
 
 
-// const isTokenExpiring = (token: string | null): boolean => {
-//   if (!token) return true;
-//   try {
-//     const { exp } = jwtDecode<JWTPayload>(token);
-//     const now = Date.now() / 1000;
-//     return exp - now < 300;
-//   } catch (e) {
-//     return true;
-//   }
-// };
+const isTokenExpiring = (token: string | null): boolean => {
+  if (!token) return true;
+  try {
+    const { exp } = jwtDecode<JWTPayload>(token);
+    const now = Date.now() / 1000;
+    return exp - now < 300;
+  } catch (e) {
+    return true;
+  }
+};
 
 
 api.interceptors.request.use(
   async (config: InternalAxiosRequestConfig) => {
     let token = localStorage.getItem("token");
     
-    // if (isTokenExpiring(token)) {
-      // try {
-      //   const response = await axios.post("/v1/auth/refresh-token", null, {
-      //     headers: {
-      //       Authorization: `Bearer ${token}`,
-      //     },
-      //   });
-      //   token = response.data.token;
-      //   localStorage.setItem("token", token!);
-      // } catch (error) {
-      // console.error("Error al refrescar token", error);
-      // localStorage.removeItem("token'"");
-      // window.location.href = '/signin';
-      // }
-    // }
+    if (isTokenExpiring(token)) {
+      try {
+
+        const dto = {
+          accessToken: token,
+          refreshToken: token
+        };
+
+
+        const response = await axios.post("/api/v1/auth/refresh-token", dto, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        token = response.data.token;
+        localStorage.setItem("token", token!);
+      } catch (error) {
+        console.error("Error al refrescar token", error);
+        localStorage.removeItem("token");
+        window.location.href = '/signin';
+      }
+    }
     
     if (token) {
       config.headers = config.headers || {};
