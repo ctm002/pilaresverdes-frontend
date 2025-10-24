d2105df16bb70efef406d75579fa2552894c045a
Carlos Rodrigo Tapia Molina <ctapia@transbank.cl>
2025-10-24 19:32:23 -0300

    remove space

--- src/EditarAviso.tsx
+++ src/EditarAviso.tsx
@@ -208,7 +208,6 @@ export default function EditarAviso() {
               />
             </div>
             
-
             <div>
               <label className="block text-gray-700 mb-1">Celular</label>
               <input
