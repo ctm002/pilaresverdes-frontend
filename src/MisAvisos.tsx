a4a92ed86000836d78954bef55ae7bfe9f6b93b9
Carlos Rodrigo Tapia Molina <ctapia@transbank.cl>
2026-04-26 13:05:19 -0400

    fix: boton

--- src/MisAvisos.tsx
+++ src/MisAvisos.tsx
@@ -31,7 +31,7 @@ export default function MisAvisos() {
 
   return (
     <div className="min-h-screen flex flex-col bg-cream">
-      {/*
+      {
       <SimpleHeader title="Mis avisos" backTo="/">
         <button
           onClick={() => navigate('/crear')}
@@ -41,7 +41,7 @@ export default function MisAvisos() {
           Publicar
         </button>
       </SimpleHeader>
-*/}
+}
       <main className="flex-grow pt-20 px-4 py-6">
 
         {/* Volver al inicio — desktop */}
